793a9467207274f803b148c8022eea8daff2bd94
yujie <2623074489@qq.com>
2020-10-27 21:56:20 +0800

    新建一个循序

--- src/main/java/com/offcn/controller/Test01Controller.java
+++ src/main/java/com/offcn/controller/Test01Controller.java
@@ -3,7 +3,11 @@ package com.offcn.controller;
 public class Test01Controller {
     public String test01(){
         //add by yujie
-        System.out.printIn("test01);
+        System.out.println("test01");
+        //add by yujie
+        while (true){
+
+        }
         return null;
     }
 }
