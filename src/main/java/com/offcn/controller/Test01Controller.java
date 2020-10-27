5927031a4962732a9492fb3bdb5405d314686a3c
ChininroYJ <73534294+ChininroYJ@users.noreply.github.com>
2020-10-27 21:46:30 +0800

    Update Test01Controller.java
    新增输出文字

--- src/main/java/com/offcn/controller/Test01Controller.java
+++ src/main/java/com/offcn/controller/Test01Controller.java
@@ -2,6 +2,8 @@ package com.offcn.controller;
 
 public class Test01Controller {
     public String test01(){
+        //add by yujie
+        System.out.printIn("test01);
         return null;
     }
 }
